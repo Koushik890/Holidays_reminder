bc2eb0f1b972f082a2a3daf1cb9a68cb118c956b
Koushik Dey <64492173+Koushik890@users.noreply.github.com>
2020-12-26 20:13:31 +0530

    Create Events.py
    Holidays name

---
+++ Events.py
@@ -0,0 +1,8 @@
+print("For which event you want to get a reminder?")
+print("Here we've a list for bunch of events.")
+print("1. New year's Eve\n")
+print("2. Guru Govind Singh Jayanti\n")
+print("3. Epiphany\n")
+print("4. Orthodox Christmas Day\n")
+print("5. Stephen Foster Memorial Day\n")
+print("6. Orthodox New Year\n")
