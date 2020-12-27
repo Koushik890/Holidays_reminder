5cb7a1a1e6bd40b48d45573054d2b754136b29a3
noob <sharmakhushal78@gmail.com>
2020-12-27 12:08:14 +0530

    added changes for holidayfinder

--- Events.py
+++ Events.py
@@ -1,8 +1,25 @@
-print("For which event you want to get a reminder?")
-print("Here we've a list for bunch of events.")
-print("1. New year's Eve\n")
-print("2. Guru Govind Singh Jayanti\n")
-print("3. Epiphany\n")
-print("4. Orthodox Christmas Day\n")
-print("5. Stephen Foster Memorial Day\n")
-print("6. Orthodox New Year\n")
+
+from datetime import date 
+from dateutil import parser
+import holidays 
+  
+# Select country 
+uk_holidays = holidays.UnitedKingdom() #this code snippet is from geeksforgeeks
+  
+# Print all the holidays in UnitedKingdom in year 2018 
+for ptr in holidays.UnitedKingdom(years = 2020).items():  
+    print(ptr) 
+
+print("\nFor which event you want reminder? Please enter the date with DD-MM-YYYY format.")
+date = parser.parse(input("Enter date: "))
+
+
+print(uk_holidays.get(date)) 
+# print("For which event you want to get a reminder?")
+# print("Here we've a list for bunch of events.")
+# print("1. New year's Eve\n")
+# print("2. Guru Govind Singh Jayanti\n")
+# print("3. Epiphany\n")
+# print("4. Orthodox Christmas Day\n")
+# print("5. Stephen Foster Memorial Day\n")
+# print("6. Orthodox New Year\n")
