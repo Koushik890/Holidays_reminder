e50b22ed0c0ad37bbeaafb66db81a351f0e9d401
Koushik Dey <64492173+Koushik890@users.noreply.github.com>
2020-12-27 20:04:31 +0530

    Final_work

--- Events.py
+++ Events.py
@@ -2,7 +2,7 @@ from validate_email import validate_email
 from datetime import date ,datetime,timedelta
 from dateutil import parser
 import holidays 
-import Email_script
+from Email_script import *
   
 # Select country 
 uk_holidays = holidays.UnitedKingdom() #this code snippet is from geeksforgeeks
@@ -16,7 +16,7 @@ while(1):
     
     remind_date = parser.parse(input("Enter date: "))
     remind_date = datetime.date(remind_date)
-    msg_seding_date = remind_date - timedelta(days = 2)
+    msg_seding_date = remind_date - timedelta(days = 5)
     todays_date = date.today()
     if uk_holidays.get(remind_date) != None :
         print('So you want to get notified for',uk_holidays.get(remind_date),"? Great! Please share your email below.")
@@ -24,16 +24,27 @@ while(1):
             email = input("Enter Email : ")
             is_valid = validate_email(email,verify=False)
             if bool(is_valid):
-                print("You are done, we'll remind you with email on", remind_date)
-                msg = "Reminder to take gifts for " + uk_holidays.get(remind_date) + " holiday."
-                # if date.today == msg_seding_date:
-                #     Email_script.email_script(email)
+                print("You are done, we'll remind you with email on", msg_seding_date)
+                message = "Reminder to take gifts for " + uk_holidays.get(remind_date) + " holiday ✨✨🎊."
+                if todays_date == msg_seding_date:
+                    email_script(email, message)
+                    
                 
                 
-                # Email_script.email_script(email,msg)
+                #Email_script.email_script(email,msg)
                 break
             else:
                 print("Email is invalid, Please enter again.")
         break
     else:
         print("This date doesn't have any holiday, please choose another date!")
+
+
+
+    
+
+
+
+
+
+
